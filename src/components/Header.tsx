f7b670efa2d686bd7561323f55a5cb4e44aa700a
iwa <iwaongit@gmail.com>
2026-04-07 22:42:56 +0200

    feat: add refresh interval information in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,12 @@ import { Link } from "@tanstack/react-router";
 import BetterAuthHeader from "./BetterAuthHeader.tsx";
 import ThemeToggle from "./ThemeToggle";
 import { Waves } from "lucide-react";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "./ui/hover-card.tsx";
+import { Button } from "./ui/button.tsx";
 
 export default function Header() {
   return (
@@ -11,6 +17,20 @@ export default function Header() {
           <Waves />
           <h2 className="m-0 text-xl font-bold tracking-light">Tidalert</h2>
         </Link>
+
+        <HoverCard openDelay={50} closeDelay={100}>
+          <HoverCardTrigger>
+            <Button size="xs" variant="outline" className="cursor-default">
+              Refresh Interval: 1h
+            </Button>
+          </HoverCardTrigger>
+          <HoverCardContent side="bottom">
+            <p className="text-sm">
+              To limit the number of requests made to the Archipelago server,
+              trackers will be refreshed every hour.
+            </p>
+          </HoverCardContent>
+        </HoverCard>
 
         <div className="ml-auto flex items-center gap-1.5 sm:ml-0 sm:gap-2">
           <BetterAuthHeader />
